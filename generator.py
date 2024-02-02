123eea9729571530728333339f0960d313d55ff3
Nick Trierweiler <35035185+ntrierw@users.noreply.github.com>
2024-02-02 20:49:19 +0000

    refactoring generator architecture for simplicity

--- generator.py
+++ generator.py
@@ -6,16 +6,6 @@ import re
 # Get the list of markdown files in content/ directory
 md_files = glob.glob("content/*.md")
 
-# Get the list of html files in template/ directory
-html_files = sorted(glob.glob("template/*.html"))
-
-# Find the index of m.html in the list
-m_index = html_files.index("template/m.html")
-
-# Split the list into two parts: before and after m.html
-before_m = html_files[:m_index]
-after_m = html_files[m_index+1:]
-
 # Specify the output directory path
 output_dir = "output"
 
@@ -37,15 +27,13 @@ for md_file in md_files:
     # Write the output file to the output directory
     with open(os.path.join(output_dir, output_file), "w") as f:
         # Prepend the html files before m.html
-        for file in before_m:
-            with open(file, "r") as g:
-                htmlRead = g.read()
-                f.write(htmlRead.replace("$title$", h1_text))
+        with open("template/top.html", "r") as g:
+            htmlRead = g.read()
+            f.write(htmlRead.replace("$title$", h1_text))
 
         # Write the converted html
         f.write(html)
 
         # Append the html files after m.html
-        for file in after_m:
-            with open(file, "r") as g:
-                f.write(g.read())
+        with open("template/bottom.html", "r") as g:
+            f.write(g.read())
